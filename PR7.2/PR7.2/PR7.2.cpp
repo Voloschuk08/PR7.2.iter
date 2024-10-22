9ca01b71d42d1c50e7fb1d4432c4907ad243805f
Voloschuk08 <voloschuk07olia@gmail.com>
2024-10-23 01:28:27 +0300

    Add files via upload

---
+++ PR7.2/PR7.2/PR7.2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void CreateMatrix(int** a, const int n, const int Low, const int High);
+void PrintMatrix(int** a, const int n);
+void TransposeMatrix(int** a, const int n);
+
+int main()
+{
+    srand((unsigned)time(NULL)); 
+
+    int Low = 1; // Нижня межа значень елементів
+    int High = 9; // Верхня межа значень елементів
+
+    int n; // Розмірність матриці
+    cout << "n: ";
+    cin >> n;
+
+    // Створення динамічної матриці розміром n x n
+    int** a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
+
+    // Створення і виведення початкової матриці
+    CreateMatrix(a, n, Low, High);
+    cout << "Original matrix:" << endl;
+    PrintMatrix(a, n);
+
+    // Транспонування матриці
+    TransposeMatrix(a, n);
+
+    // Виведення транспонованої матриці
+    cout << "Transposed matrix:" << endl;
+    PrintMatrix(a, n);
+
+    // Звільнення пам'яті
+    for (int i = 0; i < n; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
+
+// Функція для створення матриці з випадковими значеннями в межах [Low, High]
+void CreateMatrix(int** a, const int n, const int Low, const int High)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+// Функція для виведення матриці на екран у вигляді таблиці
+void PrintMatrix(int** a, const int n)
+{
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// Функція для транспонування матриці (обмін елементів matrix[i][j] з matrix[j][i])
+void TransposeMatrix(int** a, const int n)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            swap(a[i][j], a[j][i]); 
+}
---
+++ PR7.2/UnitTest7.2/UnitTest7.2.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR7.2/PR7.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72
+{
+	TEST_CLASS(UnitTest72)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int Low = 1;
+			int High = 9;
+
+			int n = 3;
+
+			// Створення динамічної матриці розміром n x n
+			int** a = new int* [n];
+			for (int i = 0; i < n; i++)
+				a[i] = new int[n];
+
+			// Вручну заповнимо матрицю для точного тестування
+			a[0][0] = 1; a[0][1] = 2; a[0][2] = 3;
+			a[1][0] = 4; a[1][1] = 5; a[1][2] = 6;
+			a[2][0] = 7; a[2][1] = 8; a[2][2] = 9;
+
+			// Очікувана транспонована матриця
+			int expected[3][3] = {
+				{1, 4, 7},
+				{2, 5, 8},
+				{3, 6, 9}
+			};
+
+			// Викликаємо функцію для транспонування
+			TransposeMatrix(a, n);
+
+			// Перевірка чи відповідає отримана матриця очікуваній
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					Assert::AreEqual(expected[i][j], a[i][j], L"Matrix element mismatch");
+				}
+			}
+
+			// Звільнення пам'яті
+			for (int i = 0; i < n; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+	};
+}	
